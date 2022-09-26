32c1a28dbf91152fb48c456d6c932e05a7fa5838
Maksim Baum <venergy-nv@ya.ru>
2022-09-26 16:19:11 +0500

    Добовление карточек на сайт из массива с помощью JS

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,33 @@ const nameInput = document.querySelector('#name');
 const jobInput = document.querySelector('#about');
 const nameProfile = profile.querySelector('.profile__title');
 const jobProfile = profile.querySelector('.profile__text');
+const libraryContainer = document.querySelector('.library');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 //функция переключения Popup`a методом скрытия
 function togglePopupClass() {
@@ -34,3 +61,12 @@ popupClose.addEventListener('click', togglePopupClass);
 
 // отправка изменений профиля на web
 formElement.addEventListener('submit', formSubmitHandler);
+
+//создание элементов card из массива
+initialCards.forEach(function(item) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__img').src = item.link;
+  cardElement.querySelector('.card__title').textContent = item.name;
+  libraryContainer.append(cardElement);
+})
